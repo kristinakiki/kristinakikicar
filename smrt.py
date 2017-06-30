eeef374ab6ab446ee2ab9f45a898d2d5e12ece72
kristinakiki <kristinapantelickika@gmail.com>
2017-06-30 08:02:44 -0700

    Add files via upload

---
+++ smrt.py
@@ -0,0 +1,18 @@
+reč1 = "pevač"
+reč2 = "beogradski"
+reć3 = "spavanje"
+
+recosnova=reč1[0:3]
+print(recosnova)
+recsufiks=reč1[3:5]
+print (recsufiks)
+
+recosnova=reč2[0:7]
+print(recosnova)
+recsufiks=reč2[7:10]
+print (recsufiks)
+
+recosnova=reć3[0:5]
+print(recosnova)
+recsufiks=reć3[5:8]
+print (recsufiks)
