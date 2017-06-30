3cc8c76b528cd9565e4518bc77085cd8da1aa5f4
kristinakiki <kristinapantelickika@gmail.com>
2017-06-30 07:24:58 -0700

    Add files via upload

---
+++ giljotina.py
@@ -0,0 +1,8 @@
+recenica='Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \ "Ju, Mitre, uplašio si me!"\. Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica.'
+recenica=recenica.lower()
+listareci=recenica.split(" ")
+print(listareci)
+
+reci=list(recenica)
+print(reci)
+
