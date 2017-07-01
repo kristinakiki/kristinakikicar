11b52e29dbaa0d77897703eeb099d5f3e733f8d4
kristinakiki <kristinapantelickika@gmail.com>
2017-07-01 08:18:46 -0700

    Add files via upload

---
+++ NESTO.py
@@ -0,0 +1,9 @@
+telefonski_imenik = {"Paja Patak": 123456, "Mini Maus": 234567, "Šilja": 345678}
+vukajlija = {"sojanica": "Posna pljeskavica. Garantovano bez trihinele.", "jahanje": "Omiljena aktivnost šefova za koju je potrebno da radnik ima konjske živce.", "šef": "Čovek koji nema smisao za umor."}
+vrste_reči = {"imenice": ["polaznik", "seminar", "lingvistika", "Isidora"], "glagoli": ["slušati", "crtati", "jesti"], "zamenice": ["on", "ona", "ono"]}
+print(telefonski_imenik["Mini Maus"])
+print(vrste_reči["imenice"])
+vukajlija["šef"] = "Miloš"
+print(vukajlija["šef"])
+print(telefonski_imenik.keys())
+print(telefonski_imenik.values())
