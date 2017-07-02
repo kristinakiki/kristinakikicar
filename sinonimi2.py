ad512bc9332b6d2e7d99881edae43ef900776883
kristinakiki <kristinapantelickika@gmail.com>
2017-07-02 07:17:18 -0700

    Add files via upload

---
+++ sinonimi2.py
@@ -0,0 +1,11 @@
+rečnik={"debeo":{"antonimi":["mrsav", "dzgoljav"],"sinonimi":["punacak", "ugojen"]}, "hladan":{"antonimi":["vruc", "topao"], "sinonimi":["studen", "leden"]}, "veseo":{"antonimi":["tuzan", "potisten"],"sinonimi":["radostan", "srecan"]}}
+print(rečnik["debeo"]["antonimi"])
+print(rečnik["hladan"]["antonimi"])
+print(rečnik["veseo"]["antonimi"])
+print(rečnik["debeo"]["sinonimi"])
+print(rečnik["hladan"]["sinonimi"])
+print(rečnik["veseo"]["sinonimi"])
+
+print(rečnik["debeo"]["sinonimi"][0])
+print(rečnik["debeo"]["antonimi"][1])
+ 
