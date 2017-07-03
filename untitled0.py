2c0fef006afd8cf81fb7f07b14e68da09974346c
kristinakiki <kristinapantelickika@gmail.com>
2017-07-03 07:32:01 -0700

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+
+
+dodole = "Oj, dodole, mili Bože\noj, dodo, dodole\nNaša doda Boga moli\noj, dodo, dodole\nSitna kiša da zarosi\noj, dodo, dodole\nNaša polja da potopi\noj, dodo, dodole\nI da rodi bel' pšenica\noj, dodo, dodole\nI kukuruz sa dva klasa\noj, dodo, dodole\nŠto molila umolila\noj, dodo, dodole\nSitna kiša zarosila\noj, dodo, dodole\nNaša polja potopila\noj, dodo, dodole\nI rodila bel' pšenica\noj, dodo, dodole\nI kukuruz sa dva klasa\noj, dodo, dodole"
+dodole1=dodole.split("\n")
+for i in range(len(dodole1)-1,0,-1):
+    print(dodole1[i])
+   
+
+
+    
+    
+
